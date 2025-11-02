3cc7f9c85eb2075527e713ac5f29d3a5cc7600f6
shahboz <shahbozabdumqjfov1122@gmail.com>
2025-11-02 14:32:28 +0500

    qwdewrgewr

--- database/seed.go
+++ database/seed.go
@@ -9,19 +9,15 @@ import (
 func SeedUserAdmin() {
 	var admin models.Admin
 
-	// Admin mavjudligini tekshiramiz
 	if err := DB.Where("role = ?", "Admin").First(&admin).Error; err != nil {
-		// Yangi admin yaratamiz
 		password := "123"
-
-		// Parolni hash qilamiz
 		hashed, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 
 		newAdmin := models.Admin{
-			Firstname: "Admin",             // ism
-			Email:     "admin@example.com", // login uchun email
+			Firstname: "Admin",
+			Email:     "admin@example.com",
 			Role:      "Admin",
-			Password:  string(hashed), // hashed parol
+			Password:  string(hashed),
 		}
 
 		if err := DB.Create(&newAdmin).Error; err != nil {
