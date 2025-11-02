4aabfb1dae0be4207edcf9a3f475ad287a70ce0f
shahboz <shahbozabdumqjfov1122@gmail.com>
2025-11-02 13:13:05 +0500

    qwdewrgewr

--- routers/router.go
+++ routers/router.go
@@ -10,28 +10,24 @@ func init() {
 	beego.Router("/", &controllers.AuthController{}, "get:GetLogin")
 
 	beego.Router("/login", &controllers.AuthController{}, "get:GetLogin;post:PostLogin")
-
 	beego.Router("/register", &controllers.AuthController{}, "get:GetRegister;post:PostRegister")
 
-	beego.Router("/dashboard/:id", &controllers.DashboardController{}, "get:Get;post:Post")
+	// Dashboard
+	beego.Router("/dashboard", &controllers.DashboardController{}, "get:Get;post:Post")
 
-	beego.Router("/dashboard", &controllers.DashboardController{}, "get:dashboard;post:dashboard")
-	// routers/router.go
-
+	// Nota operatsiyalari
 	beego.Router("/note/toggle/:id", &controllers.NoteController{}, "post:Toggle")
-
 	beego.Router("/note/delete/:id", &controllers.NoteController{}, "post:Delete")
 
-	beego.Router("/dashboard/:id", &controllers.DashboardController{}, "")
-
+	// Logout
 	beego.Router("/logout", &controllers.AuthController{}, "get:Logout")
 
+	// Qo‘shimcha sahifalar
 	beego.Router("/help", &controllers.RegisterController{}, "get:Help;post:Help")
+	beego.Router("/buyurtma", &controllers.RegisterController{}, "get:Buyurtma;post:Buyurtma")
 
-	beego.Router("/Buyurtma", &controllers.RegisterController{}, "get:Buyurtma;post:Buyurtma")
-
+	// Admin panel
 	beego.Router("/admin", &controllers.AdminController{})        // GET - adminlar ro‘yxati
 	beego.Router("/admin/create", &controllers.AdminController{}) // POST - admin yaratish
 	beego.Router("/admin/delete", &controllers.AdminController{}, "get:Delete")
-
 }
